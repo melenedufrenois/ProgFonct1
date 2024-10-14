06fdd149003ee39680d870b9bd6d6049579d907d
melenedufrenois <melenedufrenois@gmail.com>
2024-10-14 10:19:53 +0200

    single page application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 // App.js
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonList from './components/PokemonList';
+import Alphabets from './components/Alphabets';
 import './App.css';
 
 function App() {
+  const [path, setPath] = useState('/');
+
   return (
     <div className="App">
-      <PokemonList />
+      <button
+        onClick={() => {
+          const pathName = 'PokemonList';
+          setPath(`/${pathName}`);
+          window.history.pushState({ page: pathName }, pathName, `/${pathName}`);
+        }}
+      >
+        List
+      </button>
+      <button
+        onClick={() => {
+          const pathName = 'Alphabets';
+          setPath(`/${pathName}`);
+          window.history.pushState({ page: pathName }, pathName, `/${pathName}`);
+        }}
+      >
+        Alphabets
+      </button>
+      
+      <main>
+        {path === "/PokemonList" && <PokemonList />}
+        {path === "/Alphabets" && <Alphabets />}
+      </main>
     </div>
   );
 }
